48bdc8421b36d47bbbc6cb07e529dbd2030cf97e
unknown <iprincepatel22@gmail.com>
2026-02-12 01:42:51 +0530

    just added a health route

--- express_mongo_base/src/index.js
+++ express_mongo_base/src/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+// routes
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
+
 app.listen(3000, () => {
     console.log("Server running on PORT 3000");
 })
